426109773ad46d951c5c5ee6c4eb11a07f56911c
Danilo Pianini <danilo.pianini@gmail.com>
2023-01-03 19:38:25 +0100

    feat: add random generator

---
+++ src/commonMain/kotlin/org/danilopianini/rrmxmx/RrmxmxRandomSequence.kt
@@ -0,0 +1,15 @@
+package org.danilopianini.rrmxmx
+
+import org.danilopianini.rrmxmx.Rrmxmx.rrmxmx
+
+/**
+ * Generates a random sequence of ULong from a specified [seed] using rrmxmx.
+ */
+class RrmxmxRandomSequence(seed: ULong) : Sequence<ULong> by generateSequence(
+    rrmxmx(seed + gamma),
+    { rrmxmx(it + gamma) }
+) {
+    companion object {
+        private const val gamma = 0x40ead42ca1cd0131UL
+    }
+}
