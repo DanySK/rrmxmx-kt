4086d574bc17f49c7fd290930277292005d81724
Danilo Pianini <danilo.pianini@unibo.it>
2025-07-12 18:34:54 +0200

    fix(release): migrate to Maven Central Portal

--- release.config.mjs
+++ release.config.mjs
@@ -1,5 +1,5 @@
 const publishCmd = `
-./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 1
 ./gradlew publishJsPackageToNpmjsRegistry || exit 4
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
@@ -13,4 +13,4 @@ config.plugins.push(
     "@semantic-release/github",
     "@semantic-release/git",
 )
-export default config;
+export default config
